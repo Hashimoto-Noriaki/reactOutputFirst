793348d98fab928c86771a948e8decbb11ea3b97
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-02-25 02:38:40 +0900

    styles.inputをclassNameとして指定することで、スタイルが適用

--- src/components/atoms/InputForm/index.jsx
+++ src/components/atoms/InputForm/index.jsx
@@ -5,6 +5,7 @@ export const InputForm = (props) =>{
     const {inputValue,placeholder,handleChangeValue,handleKeyDown} = props;
     return (
         <input 
+        className={styles.input}
         type = "text"
         placeholder = {placeholder}
         value = {inputValue}
