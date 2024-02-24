782676cb11c5ad6e86bedede1995449050a8274e
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-02-25 02:21:03 +0900

    propsとexport追記

---
+++ src/components/atoms/InputForm/index.jsx
@@ -0,0 +1,22 @@
+import styles from './styles.module.css';
+
+export const InputForm = (props) =>{
+    const {inputValue,placeholder,handleChangeValue,handleKeyDown} = props;
+};
+
+
+
+export default InputForm;
+
+
+// export const InputForm = ({ type, placeholder, value, onChange }) => {
+//   return (
+//     <input
+//       type={type}
+//       placeholder={placeholder}
+//       value={value}
+//       onChange={onChange}
+//       className={styles.input}
+//     />
+//   );
+// }
