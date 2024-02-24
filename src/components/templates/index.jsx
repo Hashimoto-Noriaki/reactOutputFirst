46df949c991c92a41bde50acf6f07e503b17c782
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-02-25 03:11:19 +0900

    importの追記

---
+++ src/components/templates/index.jsx
@@ -0,0 +1,7 @@
+import styles from './style.module.css';
+import { InputForm } from '../../atoms/InputForm';
+import {AddTodo} from '../../organism/AddTodo';
+import {TodoList} from '../../organism/TodoList';
+
+
+
